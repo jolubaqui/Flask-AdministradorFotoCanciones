d984e04ce5d6d1c083bba2fc87968233ee877bbe
jolubaqui <jolubaqui@gmail.com>
2025-08-18 19:57:15 -0500

    Configura gunicorn

--- app/config.py
+++ app/config.py
@@ -13,13 +13,13 @@ CANCIONES_POR_PAGINA = 5
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 MAX_CONTENT_LENGTH_MB = 16
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
+SECRET_KEY = os.getenv("SECRET_KEY", "56224685ecf563087921ac5eb6e39b9905eddd0d208b661d")
 MAX_CONTENT_LENGTH = MAX_CONTENT_LENGTH_MB * 1024 * 1024
 WTF_CSRF_TIME_LIMIT = None
 
-CLOUD_NAME = os.getenv("CLOUD_NAME")
-CLOUD_API_KEY = os.getenv("CLOUD_API_KEY")
-CLOUD_API_SECRET = os.getenv("CLOUD_API_SECRET")
+CLOUD_NAME = os.getenv("dwcomv0hv")
+CLOUD_API_KEY = os.getenv("646295341746146")
+CLOUD_API_SECRET = os.getenv("0oid-hmmji7Hg8dMG6h-inbKsAw")
 
 # Asegúrate de que los directorios existan
 INSTANCE_DIR.mkdir(parents=True, exist_ok=True)
